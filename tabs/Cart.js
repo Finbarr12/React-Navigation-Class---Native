98167b48f5ec4105c5dd22b1110632d7c04479f9
finbarr12 <ogbonnafinbarr@gmail.com>
2026-07-21 16:57:01 +0100

    map

--- tabs/Cart.js
+++ tabs/Cart.js
@@ -1,10 +1,54 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 const Cart = () => {
+  const [location, setLocation] = useState();
+  const [errorMsg, setErrorMsg] = useState();
+  const [address, setAddress] = useState();
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      try {
+        let response = await fetch(
+          `https://nominatim.openstreetmap.org/reverse?lat=${location.coords.latitude}&lon=${location.coords.longitude}&format=json`,
+          {
+            headers: {
+              "User-Agent": "Location-App/1.0 (ogbonnafinbarr@gmail.com)",
+            },
+          },
+        );
+
+        let data = await response.json();
+        // console.log(data);
+        setAddress(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (address) {
+    text = address.display_name;
+  }
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Cart</Text>
+      <Text>{text}</Text>
     </View>
   );
 };
